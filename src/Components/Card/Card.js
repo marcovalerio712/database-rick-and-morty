2f6cf6760be1ac1ac41eb4ff998867852514380b
marcovalerio712 <marcovalerio99@outlook.it>
2021-12-06 21:56:46 +0100

    Added grid and cards

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Card.css";
+
+function Card(){
+    return (
+
+        <div className="card">
+            this is a card
+        </div>
+
+
+    );
+}
+
+export default Card;
---
+++ src/Components/Grid/Grid.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Grid.css";
+import Card from "../Card/Card";
+
+function Grid(){
+    return (
+
+        <div className="grid container">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            
+        </div>
+
+
+    );
+}
+
+export default Grid;
